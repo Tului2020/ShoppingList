d6de3c256ed48681ce76c6e75f5afdb415d74203
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-04 14:34:01 -0700

    able to add

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
 import Header from './compontents/Header';
 import ListItem from './compontents/ListItem';
+import AddItem from './compontents/AddItem';
+
 
 const App = () => {
   const [items, setItems] = useState([
-    { id: 1, text: 'Milk' },
-    { id: 2, text: 'Eggs' },
-    { id: 3, text: 'Bread' },
-    { id: 4, text: 'Juice' },
+    { id: Math.floor(Math.random() * 100000), text: 'Milk' },
+    { id: Math.floor(Math.random() * 100000), text: 'Eggs' },
+    { id: Math.floor(Math.random() * 100000), text: 'Bread' },
+    { id: Math.floor(Math.random() * 100000), text: 'Juice' },
   ]);
 
   const deleteItem = (id) => {
@@ -17,12 +19,18 @@ const App = () => {
     })
   }
 
+  const addItem = (text) => {
+    setItems(prevItems => {
+      return [{id: Math.floor(Math.random() * 100000), text}, ...prevItems]
+    })
+  }
 
 
 
   return (
     <View style={styles.container}>
       <Header title='Shopping List' />
+      <AddItem addItem={addItem}/>
       <FlatList
         data={items}
         renderItem={({ item }) => <ListItem item={item} deleteItem={deleteItem}/>} 
