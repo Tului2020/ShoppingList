f28dd405557ec89c3fcf6f190822e2e6f36ea668
Tului Gantulga <tgantulga@ucdavis.edu>
2021-05-04 14:16:23 -0700

    able to delete item

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, FlatList } from 'react-native';
-import Header from './compontents/Header'
-
-
+import Header from './compontents/Header';
+import ListItem from './compontents/ListItem';
 
 const App = () => {
   const [items, setItems] = useState([
@@ -10,15 +9,24 @@ const App = () => {
     { id: 2, text: 'Eggs' },
     { id: 3, text: 'Bread' },
     { id: 4, text: 'Juice' },
-  ])
+  ]);
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    })
+  }
+
+
 
 
   return (
     <View style={styles.container}>
       <Header title='Shopping List' />
-      <FlatList data={items} renderItem={({item}) =>(
-        <Text>{item.text}</Text>
-      )}/>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <ListItem item={item} deleteItem={deleteItem}/>} 
+        />
     </View>
   )
 }
